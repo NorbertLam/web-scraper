4a241199cfb384e2ca22ec1b00b961eb90b2f56d
Norbertlm <noobertlm@gmail.com>
2018-01-14 22:09:46 -0500

    update code

--- main.py
+++
@@ -1,5 +0,0 @@
-from search import search
-
-my_url = 'https://en.wikipedia.org/wiki/Volcano_tectonic_earthquake'
-
-search(my_url)
--- search.py
+++ search.py
@@ -1,37 +1,42 @@
 import urllib.request
+import requests
+import json
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-import global_file
-import re
+import sys
 
 
-def search(url):
-    if global_file.counter >= 4:
+def search(url, visited, current, target):
+    if current >= target:
         return
-    global_file.counter += 1
+    current_num = current + 1
+    target_num = target
 
     my_url = url
     url_parse = urlparse(my_url)
     url_scheme = url_parse.scheme
     url_front = url_parse.hostname
     url_end = url_parse.path
-    re_name = re.sub(r'\W+', '', url_end)
 
     my_page = urllib.request.urlopen(my_url).read()
     soup = BeautifulSoup(my_page, 'lxml')
     html_get = str(soup.prettify())
 
-    file = open("archive/" + re_name + ".html", "w", encoding='utf-8')
-    file.write(html_get)
-    file.close()
+    requests.post("http://localhost:8000/page/", json.dumps({"Title": url_end, "Body": html_get}))
 
     for line in soup.find_all('a', href=True):
 
         if '//' not in line['href'] and '#' != str(line['href'])[0] \
-                and line['href'] not in global_file.visited \
-                and ":" not in line['href']:
+                and line['href'] not in visited \
+                and ":" not in line['href'] and "=edit" not in line['href']:
 
-            global_file.visited.append(line['href'])
+            visited.append(line['href'])
             new_url = url_scheme + "://" + url_front + line['href']
 
-            search(new_url)
+            search(new_url, visited, current_num, target_num)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 0:
+        search(sys.argv[1], [], 0, int(sys.argv[2]))
+    else:
+        print("not valid")
